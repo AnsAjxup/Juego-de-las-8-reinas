a4d4459431b1cf33866da5b5e5cd95532234d81a
AnsAjxup <81263186+AnsAjxup@users.noreply.github.com>
2022-03-13 14:25:54 -0600

    Add files via upload

---
+++ 8 reinas.py
@@ -0,0 +1,53 @@
+print("Juego de las 8 reinas")
+class nreinas:
+
+    def __init__(self, tamaño):
+        self.tamaño = tamaño
+        self.results = 0
+        self.resuelve()
+
+    #inicializa la posicion, el tamaño para luego mostrar el resultado
+    def resuelve(self):
+        posicion = [-1] * self.tamaño
+        self.poner_reina(posicion, 0)
+        print("se encontraron", self.results, "soluciones")
+
+    #funcion que valida las posisciones de las reinas
+    def poner_reina(self, posicion, fila_objetivo):
+        if fila_objetivo == self.tamaño:
+            self.muestra_todo_tablero(posicion)
+            self.results += 1
+        else:
+            for columna in range(self.tamaño):
+                #cancela las posiciones invalidas
+                if self.check_place(posicion, fila_objetivo, columna):
+                    posicion[fila_objetivo] = columna
+                    self.poner_reina(posicion, fila_objetivo + 1)
+
+    #revisa que no haya reinas bajo ataque
+    def check_place(self, posicion, fila_ocupada, columna):
+        for i in range(fila_ocupada):
+            if posicion[i] == columna or \
+                posicion[i] - i == columna - fila_ocupada or \
+                posicion[i] + i == columna + fila_ocupada:
+
+                return False
+        return True
+
+    #funcion que dibuja el tablero
+    def muestra_todo_tablero(self, posicion):
+        for row in range(self.tamaño):
+            line = ""
+            for columna in range(self.tamaño):
+                if posicion[row] == columna:
+                    line += "R "
+                else:
+                    line += ". "
+            print(line)
+        print("\n")
+
+def main():
+    nreinas(8) #nuemero de reinas
+
+if __name__ == "__main__":
+    main()
